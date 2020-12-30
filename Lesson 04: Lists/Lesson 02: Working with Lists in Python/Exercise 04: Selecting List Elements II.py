b230c203332f89291cd3cce4ead34dd6ddb5c8b7
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 17:06:51 -0600

    Create Exercise 04: Selecting List Elements II.py

---
+++ Lesson 04: Lists/Lesson 02: Working with Lists in Python/Exercise 04: Selecting List Elements II.py
@@ -0,0 +1,15 @@
+# We can use the index -1 to select the last item of a list,
+# even when we don’t know how many elements are in a list.
+
+shopping_list = ['eggs', 'butter', 'milk', 'cucumbers', 'juice', 'cereal']
+
+print(len(shopping_list))
+
+# Get last element
+last_element = shopping_list[-1]
+
+# Get 5th element
+element5 = shopping_list[5]
+
+print(element5)
+print(last_element)
