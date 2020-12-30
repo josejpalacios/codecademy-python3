fc642e89bf9a170ce7d6e917b92a836905377b6b
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 16:22:49 -0600

    Create Exercise 10: Review.py

---
+++ Lesson 04: Lists/Lesson 01: Creating and Modifying a List in Python/Exercise 10: Review.py
@@ -0,0 +1,16 @@
+# Create list
+first_names = ['Ainsley', 'Ben', 'Chani', 'Depak']
+
+# Create empty list
+age = []
+# Append to age
+age.append(42)
+
+# Create and add two list
+all_ages = [32, 41, 29] + age
+
+# Create zip using first names and all ages
+name_and_age = zip(first_names, all_ages)
+
+# Create range from 0 to 3
+ids = range(4)
