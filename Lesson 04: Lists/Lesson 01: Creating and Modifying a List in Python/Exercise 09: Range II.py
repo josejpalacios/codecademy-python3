6f082c28408d8d9f072bbc7184ad0758803fdd8b
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 16:17:33 -0600

    Create Exercise 09: Range II.py

---
+++ Lesson 04: Lists/Lesson 01: Creating and Modifying a List in Python/Exercise 09: Range II.py
@@ -0,0 +1,15 @@
+# If we call range with two arguments, we can create a list that starts at a different number.
+
+# With one or two arguments, range will create a list of consecutive numbers (i.e., each number is one greater than the previous number).
+
+# If we use a third argument, we can create a list that “skips” numbers.
+
+# Starts at 5
+# Stops at 15
+# Skip 3 numbers
+list1 = range(5, 15, 3)
+
+# Starts at 0
+# Stops at 40
+# Skip 5 numbers
+list2 = range(0,40, 5)
