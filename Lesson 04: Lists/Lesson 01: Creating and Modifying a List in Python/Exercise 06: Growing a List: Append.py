6cc6e5b24dc57b46dfafc3ca13a4a7d977b43e8d
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 16:04:51 -0600

    Create Exercise 06: Growing a List: Append.py

---
+++ Lesson 04: Lists/Lesson 01: Creating and Modifying a List in Python/Exercise 06: Growing a List: Append.py
@@ -0,0 +1,11 @@
+# We can add a single element to a list using .append()
+
+# When we use .append() on a list that already has elements, our new element is added to the end of the list.
+orders = ['daisies', 'periwinkle']
+
+print(orders)
+
+orders.append('tulips')
+orders.append('roses')
+
+print(orders)
