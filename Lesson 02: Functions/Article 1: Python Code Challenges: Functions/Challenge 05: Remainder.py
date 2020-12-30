2d628b108b5fc21feb2004b6a3772c620432f340
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 14:30:18 -0600

    Update and rename Challenge 05: Remainder to Challenge 05: Remainder.py

--- Lesson 02: Functions/Article 1: Python Code Challenges: Functions/Challenge 05: Remainder.py
+++ Lesson 02: Functions/Article 1: Python Code Challenges: Functions/Challenge 05: Remainder.py
@@ -1,6 +1,6 @@
-Write a function named remainder() that has two parameters named num1 and num2.
+# Write a function named remainder() that has two parameters named num1 and num2.
 
-The function should return the remainder of twice num1 divided by half of num2.
+# The function should return the remainder of twice num1 divided by half of num2.
 
 # Write your remainder function here:
 def remainder(num1, num2):
