3027bd00f32d7f9ddbd90f10bd6b4832db84855d
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 15:53:39 -0600

    Create Exercise 02: Lists II.py

---
+++ Lesson 04: Lists/Lesson 01: Creating and Modifying a List in Python/Exercise 02: Lists II.py
@@ -0,0 +1,7 @@
+# Lists can contain more than just numbers.
+
+# We can also combine multiple data types in one list.
+
+ints_and_strings = [1, 2, 3, 'four', 'five', 'six']
+
+sam_height = ['Sam', 67]
