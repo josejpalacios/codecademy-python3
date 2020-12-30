77abeaeb0980c46b1e452930a59e01004e8045d7
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 14:31:23 -0600

    Update and rename Challenge 02: Tip to Challenge 02: Tip.py

--- Lesson 02: Functions/Article 2: Advance Python Code Challenges: Functions/Challenge 02: Tip.py
+++ Lesson 02: Functions/Article 2: Advance Python Code Challenges: Functions/Challenge 02: Tip.py
@@ -1,6 +1,6 @@
-Create a function called tip() that has two parameters named total and percentage.
+# Create a function called tip() that has two parameters named total and percentage.
 
-This function should return the amount you should tip given a total and the percentage you want to tip.
+# This function should return the amount you should tip given a total and the percentage you want to tip.
 
 # Write your tip function here:
 def tip(total, percentage):
