66afab47116ecc2766d21fb9b4dc7942139ce533
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 16:52:01 -0600

    Create Exercise 01: Operations on Lists.py

---
+++ Lesson 04: Lists/Lesson 02: Working with Lists in Python/Exercise 01: Operations on Lists.py
@@ -0,0 +1,7 @@
+# We can start working with existing lists of data.
+
+# In this lesson, you’ll learn how to:
+# - Get the length of a list
+# - Select subsets of a list (called slicing)
+# - Count the number of times that an element appears in a list
+# - Sort a list of items
