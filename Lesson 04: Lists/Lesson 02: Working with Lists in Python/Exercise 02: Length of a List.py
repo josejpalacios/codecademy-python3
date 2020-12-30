db3099481a2b2ae8e0d61309c500ff2e6eb33b45
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 16:55:14 -0600

    Create Exercise 02: Length of a List.py

---
+++ Lesson 04: Lists/Lesson 02: Working with Lists in Python/Exercise 02: Length of a List.py
@@ -0,0 +1,11 @@
+# We’ll need to find the number of items in a list, usually called its length
+
+# When we apply len to a list, we get the number of elements in that list
+
+# Changed 3 from 2
+list1 = range(2, 20, 3)
+
+# Length of list1
+list1_len = len(list1)
+
+print(list1_len)
