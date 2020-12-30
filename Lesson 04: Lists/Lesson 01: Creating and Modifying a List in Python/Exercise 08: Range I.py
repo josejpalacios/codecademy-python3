30ab17face9306894fe09abf37e06e6b8e651988
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 16:13:01 -0600

    Create Exercise 08: Range I.py

---
+++ Lesson 04: Lists/Lesson 01: Creating and Modifying a List in Python/Exercise 08: Range I.py
@@ -0,0 +1,6 @@
+# The function range takes a single input, and generates numbers starting at 0 and ending at the number before the input. 
+
+# The range function returns an object that we can convert into a list.
+
+list1 = range(9)
+list2 = range(8)
