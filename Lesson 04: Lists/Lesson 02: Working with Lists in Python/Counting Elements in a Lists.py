abc313c203ce7f2cd1b336a9a883c0d4f0ef9a7f
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 17:19:30 -0600

    Create Counting Elements in a Lists.py

---
+++ Lesson 04: Lists/Lesson 02: Working with Lists in Python/Counting Elements in a Lists.py
@@ -0,0 +1,8 @@
+# If we want to know how many times X appears in a list, we can use the function count
+
+votes = ['Jake', 'Jake', 'Laurie', 'Laurie', 'Laurie', 'Jake', 'Jake', 'Jake', 'Laurie', 'Cassie', 'Cassie', 'Jake', 'Jake', 'Cassie', 'Laurie', 'Cassie', 'Jake', 'Jake', 'Cassie', 'Laurie']
+
+# Count Jake
+jake_votes = votes.count('Jake')
+
+print(jake_votes)
