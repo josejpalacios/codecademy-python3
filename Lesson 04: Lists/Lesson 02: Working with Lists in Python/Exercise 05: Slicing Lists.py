dd6b50d244e24ccf7ec97fc5a3ce367648f2a2b8
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 17:13:18 -0600

    Create Exercise 05: Slicing Lists.py

---
+++ Lesson 04: Lists/Lesson 02: Working with Lists in Python/Exercise 05: Slicing Lists.py
@@ -0,0 +1,15 @@
+# Creating a selection from a list is called slicing.
+
+# We can do this using the following syntax: letters[start:end].
+
+# start is the index of the first element that we want to include in our selection.
+
+# end is the index of one more than the last index that we want to include.
+
+suitcase = ['shirt', 'shirt', 'pants', 'pants', 'pajamas', 'books']
+
+beginning = suitcase[0:4]
+print(beginning)
+
+# List with two middle items from suitcase
+middle = suitcase[2:4]
