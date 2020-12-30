282971a2acca4973ad47773a2fed7766974efeac
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 17:17:06 -0600

    Create Exercise 06: Slicing Lists II.py

---
+++ Lesson 04: Lists/Lesson 02: Working with Lists in Python/Exercise 06: Slicing Lists II.py
@@ -0,0 +1,13 @@
+# When starting at the beginning of the list, it is also valid to omit the 0
+
+# We can omit the final index when selecting the final elements from a list.
+
+# We can use negative indexes to count backward from the last element.
+
+suitcase = ['shirt', 'shirt', 'pants', 'pants', 'pajamas', 'books']
+
+# First 3 elements
+start = suitcase[:3]
+
+# Last 2 elements
+end = suitcase[-2:]
