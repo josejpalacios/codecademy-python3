fa6db9e1b0d69ec0884f99441ed69ff9621a3a0f
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 14:37:39 -0600

    Update and rename Exercise 01: Introduction to Functions to Exercise 01: Introduction to Functions.py

---
+++ Lesson 02: Functions/Lesson 1: Introduction to Functions/Exercise 01: Introduction to Functions.py
@@ -0,0 +1,4 @@
+# A function is a collection of several lines of code.
+# By calling a function, we can call all of these lines of code at once, without having to repeat ourselves.
+
+# So, a function is a tool that you can use over and over again to produce consistent output from different inputs.
