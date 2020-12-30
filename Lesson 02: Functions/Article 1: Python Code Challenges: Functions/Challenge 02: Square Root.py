968056655ad514ec46d650312817635b46ac492f
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 14:29:11 -0600

    Update and rename Challenge 02: Square Root to Challenge 02: Square Root.py

--- Lesson 02: Functions/Article 1: Python Code Challenges: Functions/Challenge 02: Square Root.py
+++ Lesson 02: Functions/Article 1: Python Code Challenges: Functions/Challenge 02: Square Root.py
@@ -1,6 +1,6 @@
-Write a function named square_root() that has one parameter named num.
+# Write a function named square_root() that has one parameter named num.
 
-Use exponents (**) to return the square root of num.
+# Use exponents (**) to return the square root of num.
 
 # Write your square_root function here:
 def square_root(num):
