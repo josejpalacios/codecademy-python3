e7fab9086c3d5878efd525a264e6b8c610c0f7e0
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 14:31:47 -0600

    Update and rename Challenge 03: Bond, James Bond to Challenge 03: Bond, James Bond.py

--- Lesson 02: Functions/Article 2: Advance Python Code Challenges: Functions/Challenge 03: Bond, James Bond.py
+++ Lesson 02: Functions/Article 2: Advance Python Code Challenges: Functions/Challenge 03: Bond, James Bond.py
@@ -1,6 +1,6 @@
-Write a function named introduction() that has two parameters named first_name and last_name.
+# Write a function named introduction() that has two parameters named first_name and last_name.
 
-The function should return the last_name, followed by a comma, a space, first_name another space, and finally last_name.
+# The function should return the last_name, followed by a comma, a space, first_name another space, and finally last_name.
 
 # Write your introduction function here:
 def introduction(first_name, last_name):
