abc76c2ef4356a52668779373231c02c8a63d8b8
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 14:49:18 -0600

    Update and rename Challenge 02: Over Budget to Challenge 02: Over Budget.py

--- Lesson 03: Control Flow/Article 1: Python Code Challenges: Control Flow/Challenge 02: Over Budget.py
+++ Lesson 03: Control Flow/Article 1: Python Code Challenges: Control Flow/Challenge 02: Over Budget.py
@@ -1,6 +1,6 @@
-Create a function called over_budget that has five parameters named budget, food_bill, electricity_bill, internet_bill, and rent.
+# Create a function called over_budget that has five parameters named budget, food_bill, electricity_bill, internet_bill, and rent.
 
-The function should return True if budget is less than the sum of the other four parameters — you’ve gone over budget! Return False otherwise.
+# The function should return True if budget is less than the sum of the other four parameters — you’ve gone over budget! Return False otherwise.
 
 # Write your over_budget function here:
 def over_budget(budget, food_bill, electricity_bill, internet_bill, rent):
