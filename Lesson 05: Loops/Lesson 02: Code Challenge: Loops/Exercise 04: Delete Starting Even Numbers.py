693aa1180c5ceb0d868e24af2070cc9416f88fce
josejpalacios <jose.palacios6119@gmail.com>
2020-12-31 00:44:06 -0600

    Update Exercise 04: Delete Starting Even Numbers.py

--- Lesson 05: Loops/Lesson 02: Code Challenge: Loops/Exercise 04: Delete Starting Even Numbers.py
+++ Lesson 05: Loops/Lesson 02: Code Challenge: Loops/Exercise 04: Delete Starting Even Numbers.py
@@ -5,3 +5,22 @@
 # For example if lst started as [4, 8, 10, 11, 12, 15], then delete_starting_evens(lst) should return [11, 12, 15].
 
 # Make sure your function works even if every element in the list is even!
+
+#Write your function here
+def delete_starting_evens(lst):
+  # While lst contains a number
+  while(len(lst) > 0):
+    # If first number is even
+    if(lst[0] % 2 == 0):
+      # Remove first number using slicing
+      lst = lst[1:]
+    # Else first number is odd
+    else:
+      # Break out of loop
+      break 
+  # Return lst
+  return lst
+
+#Uncomment the lines below when your function is done
+print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
+print(delete_starting_evens([4, 8, 10]))
