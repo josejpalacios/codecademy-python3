42e4bce48ff7789903017a8d9fc4672cb55fa7a8
josejpalacios <jose.palacios6119@gmail.com>
2020-12-31 02:21:05 -0600

    Update Exercise 02: They're All Lists!.py

--- Lesson 06: Strings/Lesson 01: Introduction to Strings/Exercise 02: They're All Lists!.py
+++ Lesson 06: Strings/Lesson 01: Introduction to Strings/Exercise 02: They're All Lists!.py
@@ -1,1 +1,10 @@
+# A string can be thought of as a list of characters.
 
+# Like any other list, each character in a string has an index.
+
+# It’s important to note that indices of strings must be integers.
+
+#If you were to try to select a non-integer index you would get a TypeError.
+
+my_name = 'Jose'
+first_initial = my_name[0]
