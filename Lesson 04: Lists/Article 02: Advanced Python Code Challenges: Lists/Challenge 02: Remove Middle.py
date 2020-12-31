9984a69c984b835b177467f0fa0d16cc6775c119
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 19:09:23 -0600

    Update Challenge 02: Remove Middle.py

--- Lesson 04: Lists/Article 02: Advanced Python Code Challenges: Lists/Challenge 02: Remove Middle.py
+++ Lesson 04: Lists/Article 02: Advanced Python Code Challenges: Lists/Challenge 02: Remove Middle.py
@@ -5,3 +5,17 @@
 # For example, the following code should return [4, 23, 42] because elements at indices 1, 2, and 3 have been removed:
 
 # remove_middle([4, 8 , 15, 16, 23, 42], 1, 3)
+
+#Write your function here
+def remove_middle(lst, start, end):
+  # Slice from first element to start
+  lst_start = lst[:start]
+  # Slice from end+1 to last element
+  lst_end = lst[end+1:]
+  # Add both list together
+  new_lst = lst_start + lst_end
+  # Return new list
+  return new_lst
+
+#Uncomment the line below when your function is done
+print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
