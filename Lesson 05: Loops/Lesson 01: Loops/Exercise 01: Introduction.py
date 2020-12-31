076fe2c6902336f5e69ffd7ec1cdc7bc8964f110
josejpalacios <jose.palacios6119@gmail.com>
2020-12-30 20:57:28 -0600

    Update and rename Exercise 01: to Exercise 01: Introduction.py

---
+++ Lesson 05: Loops/Lesson 01: Loops/Exercise 01: Introduction.py
@@ -0,0 +1,14 @@
+# A loop is a way of repeating a set of code many times.
+
+dog_breeds = ['french_bulldog', 'dalmatian', 'shihtzu', 'poodle', 'collie']
+
+# Can rewrite this code
+# print(dog_breeds[0])
+# print(dog_breeds[1])
+# print(dog_breeds[2])
+# print(dog_breeds[3])
+# print(dog_breeds[4])
+
+# For loop
+for breed in dog_breeds:
+    print(breed)
