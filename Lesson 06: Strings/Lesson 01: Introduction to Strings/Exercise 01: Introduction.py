51e19b87e57a11df4ae552f2f7ebe3fda542e4c1
josejpalacios <jose.palacios6119@gmail.com>
2020-12-31 02:17:33 -0600

    Update and rename Exercise 01.py to Exercise 01: Introduction.py

---
+++ Lesson 06: Strings/Lesson 01: Introduction to Strings/Exercise 01: Introduction.py
@@ -0,0 +1,6 @@
+# In Python, the way we store something like a word, a sentence, or even a whole paragraph is as a string.
+# A string is a sequence of characters contained within a pair of 'single quotes' or "double quotes".
+# A string can be any length and can contain any letters, numbers, symbols, and spaces.
+
+favorite_word = 'python'
+print(favorite_word)
