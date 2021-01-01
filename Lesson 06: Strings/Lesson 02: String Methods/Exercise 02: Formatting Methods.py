9e06437eb50d580e06783d83063ebc213246e8a4
josejpalacios <jose.palacios6119@gmail.com>
2021-01-01 01:11:21 -0600

    Create Exercise 02: Formatting Methods.py

---
+++ Lesson 06: Strings/Lesson 02: String Methods/Exercise 02: Formatting Methods.py
@@ -0,0 +1,20 @@
+# .lower() returns the string with all lowercase characters.
+# .upper() returns the string with all uppercase characters.
+# .title() returns the string in title case, which means the first letter of each word is capitalized.
+
+poem_title = "spring storm"
+poem_author = "William Carlos Williams"
+
+#Use .title()
+poem_title_fixed = poem_title.title()
+# Print differences
+print(poem_title)
+print(poem_title_fixed)
+# Second string has first letter of both words capitalized
+
+# Use .upper()
+poem_author_fixed = poem_author.upper()
+# Print differences
+print(poem_author)
+print(poem_author_fixed)
+# Second string is all capitalized
