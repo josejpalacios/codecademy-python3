c956e2b454b35b21d2aec53e76bfca084d3bf9e3
josejpalacios <jose.palacios6119@gmail.com>
2021-01-01 00:42:29 -0600

    Create Exercise 01: Introduction.py

---
+++ Lesson 06: Strings/Lesson 02: String Methods/Exercise 01: Introduction.py
@@ -0,0 +1,5 @@
+# Python comes with built-in string methods that gives you
+# the power to perform complicated tasks on strings very quickly and efficiently. 
+
+# A string method is called at the end of a string
+# and each one has its own method specific arguments.
