c069d93d3774e026b21b4ce972ddb052a9ea4045
josejpalacios <jose.palacios6119@gmail.com>
2021-01-01 13:09:47 -0600

    Update Exercise 03: Splitting Strings.py

--- Lesson 06: Strings/Lesson 02: String Methods/Exercise 03: Splitting Strings.py
+++ Lesson 06: Strings/Lesson 02: String Methods/Exercise 03: Splitting Strings.py
@@ -1,1 +1,7 @@
+# .split() is performed on a string, takes one argument,
+# and returns a list of substrings found between the given argument
+# (which in the case of .split() is known as the delimiter).
 
+line_one = "The sky has given over"
+# Use .split()
+line_one_words = line_one.split()
