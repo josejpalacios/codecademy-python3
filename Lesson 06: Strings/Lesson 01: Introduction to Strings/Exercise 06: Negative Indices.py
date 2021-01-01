e8039e34a56758c872f4ff12d566516871ac1bf5
josejpalacios <jose.palacios6119@gmail.com>
2020-12-31 23:10:33 -0600

    Create Exercise 06: Negative Indices.py

---
+++ Lesson 06: Strings/Lesson 01: Introduction to Strings/Exercise 06: Negative Indices.py
@@ -0,0 +1,9 @@
+# Negative indices count backward from the end of the string,
+# so string_name[-1] is the last character of the string,
+# string_name[-2] is the second last character of the string, etc.
+
+company_motto = "Copeland's Corporate Company helps you capably cope with the constant cacophony of daily life"
+
+# Use negative index
+second_to_last = company_motto[-2]
+final_word = company_motto[-4:]
