c1dd77f0f4666c28bc5052065de4e43f54f45350
josejpalacios <jose.palacios6119@gmail.com>
2020-12-31 23:22:54 -0600

    Create Exercise 08: Escape Characters.py

---
+++ Lesson 06: Strings/Lesson 01: Introduction to Strings/Exercise 08: Escape Characters.py
@@ -0,0 +1,7 @@
+# By adding a backslash in front of the special character
+# we want to escape, \", we can include it in a string.
+
+# Fix string with backslash \
+# password = "theycallme"crazy"91"
+
+password = "theycallme\"crazy\"91"
