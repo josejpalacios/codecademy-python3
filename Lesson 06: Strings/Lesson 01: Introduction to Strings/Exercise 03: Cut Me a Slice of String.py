455cf8dbc05b4029ab2de29c966f1b266d7eddb2
josejpalacios <jose.palacios6119@gmail.com>
2020-12-31 18:37:36 -0600

    Update Exercise 03: Cut Me a Slice of String.py

--- Lesson 06: Strings/Lesson 01: Introduction to Strings/Exercise 03: Cut Me a Slice of String.py
+++ Lesson 06: Strings/Lesson 01: Introduction to Strings/Exercise 03: Cut Me a Slice of String.py
@@ -1,1 +1,14 @@
+# This is called slicing a string.
+# When we slice a string we are creating a new string that
+# starts at (and includes) the first_index and ends at (but excludes) the last_index.
 
+# We can also have open-ended selections.
+# If we remove the first index, the slice starts at the beginning of the string
+# and if we remove the second index the slice continues to the end of the string.
+
+first_name = "Rodrigo"
+last_name = "Villanueva"
+
+# Slicing strings
+new_account = last_name[:5]
+temp_password = last_name[2:6]
