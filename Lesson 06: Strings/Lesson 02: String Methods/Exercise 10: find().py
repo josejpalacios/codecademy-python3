8bc6b26782ed7e1763809c49252a64be6797d7d2
josejpalacios <jose.palacios6119@gmail.com>
2021-01-01 13:57:16 -0600

    Create Exercise 10: find().py

---
+++ Lesson 06: Strings/Lesson 02: String Methods/Exercise 10: find().py
@@ -0,0 +1,8 @@
+# .find() takes a string as an argument and
+# searching the string it was run on for that string.
+# It then returns the first index value where that string is located.
+
+god_wills_it_line_one = "The very earth will disown you"
+
+# Use .find(search)
+disown_placement = god_wills_it_line_one.find("disown")
