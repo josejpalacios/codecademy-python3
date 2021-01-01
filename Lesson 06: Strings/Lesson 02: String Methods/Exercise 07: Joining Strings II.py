b883e485d516470c51f74c70bc5075548bc5acfc
josejpalacios <jose.palacios6119@gmail.com>
2021-01-01 13:33:11 -0600

    Create Exercise 07: Joining Strings II.py

---
+++ Lesson 06: Strings/Lesson 02: String Methods/Exercise 07: Joining Strings II.py
@@ -0,0 +1,6 @@
+#  You can use any string as a delimiter to join together a list of strings.
+
+winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the disattiring are completed!', 'A liquid moon', 'moves gently among', 'the long branches.', 'Thus having prepared their buds', 'against a sure winter', 'the wise trees', 'stand sleeping in the cold.']
+
+winter_trees_full = "\n".join(winter_trees_lines)
+print(winter_trees_full)
