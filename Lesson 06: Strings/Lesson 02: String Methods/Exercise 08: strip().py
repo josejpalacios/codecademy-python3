f3c3b723c6eeaa894eba122321110163dc410ffb
josejpalacios <jose.palacios6119@gmail.com>
2021-01-01 13:49:08 -0600

    Create Exercise 08: strip().py

---
+++ Lesson 06: Strings/Lesson 02: String Methods/Exercise 08: strip().py
@@ -0,0 +1,9 @@
+# .strip() removes all whitespace characters from the beginning and end.
+
+# You can also use .strip() with a character argument,
+# which will strip that character from either end of the string.
+
+love_maybe_lines = ['Always    ', '     in the middle of our bloodiest battles  ', 'you lay down your arms', '           like flowering mines    ','\n' ,'   to conquer me home.    ']
+
+# Strip whitespace
+love_maybe_lines_stripped= [line.strip() for line in love_maybe_lines]
