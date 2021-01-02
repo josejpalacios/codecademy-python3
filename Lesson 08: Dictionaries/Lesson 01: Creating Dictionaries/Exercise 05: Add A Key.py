bc0bc8cbdeac3017490c5b93fb31576ae226a296
josejpalacios <jose.palacios6119@gmail.com>
2021-01-02 14:36:32 -0600

    Create Exercise 05: Add A Key.py

---
+++ Lesson 08: Dictionaries/Lesson 01: Creating Dictionaries/Exercise 05: Add A Key.py
@@ -0,0 +1,10 @@
+# Create empty list
+animals_in_zoo = {}
+# Add zebras
+animals_in_zoo["zebras"] = 8
+# Add key value pair
+animals_in_zoo["monkeys"] = 12
+# Add dinosaurs
+animals_in_zoo["dinosaurs"] = 0
+# Print dictionary
+print(animals_in_zoo)
