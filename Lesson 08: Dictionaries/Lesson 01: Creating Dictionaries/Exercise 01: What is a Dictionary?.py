c47d6d2979555c9f1256a9f8112bb714e9a91616
josejpalacios <jose.palacios6119@gmail.com>
2021-01-01 19:18:33 -0600

    Update Exercise 01: What is a Dictionary?.py

--- Lesson 08: Dictionaries/Lesson 01: Creating Dictionaries/Exercise 01: What is a Dictionary?.py
+++ Lesson 08: Dictionaries/Lesson 01: Creating Dictionaries/Exercise 01: What is a Dictionary?.py
@@ -4,3 +4,9 @@
 # Each item consists of a key (i.e., “oatmeal”) and a value (i.e., 3)
 # Each key: value pair (i.e., "oatmeal": 3 or "avocado toast": 6) is separated by a comma (,)
 # It’s considered good practice to insert a space () after each comma, but your code will still run without the space.
+
+sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry":22}
+# SyntaxError: invalid syntax. Fixed with adding commas
+num_cameras = {"backyard": 6,  "garage": 2, "driveway": 1}
+
+print(sensors)
