434eb64f487b58683e3dcaf3289d216aa7e58776
josejpalacios <jose.palacios6119@gmail.com>
2021-01-02 14:42:52 -0600

    Create Exercise 07: Overwrite Values.py

---
+++ Lesson 08: Dictionaries/Lesson 01: Creating Dictionaries/Exercise 07: Overwrite Values.py
@@ -0,0 +1,5 @@
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+# Add key value pair
+oscar_winners["Supporting Actress"] = "Viola Davis"
+# # Change value of Best Picture
+oscar_winners["Best Picture"] = "Moonlight"
