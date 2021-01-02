3ea63fd31bbd9c6ed33c5d7979aebed260627440
josejpalacios <jose.palacios6119@gmail.com>
2021-01-02 15:34:08 -0600

    Update and rename Exercise 01: Get A Key.py to Exercise 01: Using Dictionaries.py

---
+++ Lesson 08: Dictionaries/Lesson 02: Using Dictionaries/Exercise 01: Using Dictionaries.py
@@ -0,0 +1,5 @@
+# Can use created dictionaries to solve problems.
+# Use a key to get a value from a dictionary
+# Check for existence of keys
+# Find the length of a dictionary
+# Iterate through keys and values in dictionaries
