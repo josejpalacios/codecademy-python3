00817a8e44fc76d9e22eb3b8b3f7b56613e69200
josejpalacios <jose.palacios6119@gmail.com>
2021-01-02 15:32:49 -0600

    Create Exercise 02: Get A Key.py

---
+++ Lesson 08: Dictionaries/Lesson 02: Using Dictionaries/Exercise 02: Get A Key.py
@@ -0,0 +1,5 @@
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+# Print value of key earth
+print(zodiac_elements["earth"])
+# Print value of key fire
+print(zodiac_elements["fire"])
