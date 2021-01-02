88d0f55862fc0abeb0523f4c14963355c8bd23b4
josejpalacios <jose.palacios6119@gmail.com>
2021-01-01 19:10:18 -0600

    Create Exercise 06: Modules Python Review.py

---
+++ Lesson 07: Modules/Lesson 01: Modules Python/Exercise 06: Modules Python Review.py
@@ -0,0 +1,3 @@
+# With the power of modules, we can import any code that someone else has shared publicly.
+
+# Using a package manager (like conda or pip3), you can install any modules available on the Python Package Index.
