7c969f42b7277d6dd8d10e5b1cbcdf74faa9595f
josejpalacios <jose.palacios6119@gmail.com>
2021-01-03 13:41:44 -0600

    Create Exercise 03: Instantiation.py

---
+++ Lesson 10: Classes/Lesson 01: Learn Python: Classes/Exercise 03: Instantiation.py
@@ -0,0 +1,9 @@
+# A class must be instantiated. 
+# We must create an instance of the class,
+# in order to breathe life into the schematic.
+
+class Facade:
+  pass
+
+# Create instance of Facade class
+facade_1 = Facade()
