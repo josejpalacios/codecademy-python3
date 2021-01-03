13b37ab3087f99ce4cf6179d7e278fd0ff550da5
josejpalacios <jose.palacios6119@gmail.com>
2021-01-03 14:17:06 -0600

    Create Exercise 06: Methods.py

---
+++ Lesson 10: Classes/Lesson 01: Learn Python: Classes/Exercise 06: Methods.py
@@ -0,0 +1,13 @@
+# Methods are functions that are defined as part of a class.
+# The first argument in a method is always the object that is calling the method.
+# Convention recommends that we name this first argument self.
+# Methods always have at least this one argument.
+
+# We define methods similarly to functions, except that they are indented to be part of the class.
+
+# Create class
+class Rules:
+  # Create method
+  def washing_brushes(self):
+    # Return message
+    return "Point bristles towards the basin while washing your brushes."
