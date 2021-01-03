406709d5a8452b8d1292bc7cd2dcb26f89d6be18
josejpalacios <jose.palacios6119@gmail.com>
2021-01-03 13:52:11 -0600

    Create Exercise 05: Class Variables.py

---
+++ Lesson 10: Classes/Lesson 01: Learn Python: Classes/Exercise 05: Class Variables.py
@@ -0,0 +1,9 @@
+# A class variable is a variable that’s the same for every instance of the class.
+
+# You can define a class variable by including it in the indented part of your class definition,
+# and you can access all of an object’s class variables with object.variable syntax.
+
+# Create class
+class Grade:
+  # Create class attribute
+  minimum_passing = 65
