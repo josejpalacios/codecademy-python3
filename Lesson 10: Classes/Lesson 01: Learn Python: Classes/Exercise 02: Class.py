ac34f12b7ada548e38c7bff41bea0e045dbf7d92
josejpalacios <jose.palacios6119@gmail.com>
2021-01-03 13:39:52 -0600

    Create Exercise 02: Class.py

---
+++ Lesson 10: Classes/Lesson 01: Learn Python: Classes/Exercise 02: Class.py
@@ -0,0 +1,11 @@
+# A class is a template for a data type.
+# It describes the kinds of information that class will hold
+# and how a programmer will interact with that data.
+# Define a class using the class keyword.
+
+# PEP 8 Style Guide for Python Code recommends capitalizing the
+# names of classes to make them easier to identify.
+
+# Create empty class
+class Facade:
+  pass
