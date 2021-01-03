488e819c5a74a6e88ea76ea06d9882d6cb44b13f
josejpalacios <jose.palacios6119@gmail.com>
2021-01-03 13:37:41 -0600

    Update Exercise 01: Types.py

--- Lesson 10: Classes/Lesson 01: Learn Python: Classes/Exercise 01: Types.py
+++ Lesson 10: Classes/Lesson 01: Learn Python: Classes/Exercise 01: Types.py
@@ -1,1 +1,15 @@
+# Float vs Int
+# List vs Dict
 
+# We can check the type of a Python variable using the type() function.
+
+# Print type of 5
+print(type(5))
+# Create dictionary
+my_dict = {}
+# Print type of my_dict
+print(type(my_dict))
+# Create list
+my_list = []
+# Print type of my_list
+print(type(my_list))
