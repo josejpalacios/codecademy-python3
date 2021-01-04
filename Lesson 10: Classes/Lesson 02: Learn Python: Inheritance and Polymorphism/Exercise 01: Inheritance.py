888d190b09f3e750f72e6813c6ebdfd05a2227b5
josejpalacios <jose.palacios6119@gmail.com>
2021-01-03 22:37:04 -0600

    Update and rename Exercise 01:.py to Exercise 01: Inheritance.py

---
+++ Lesson 10: Classes/Lesson 02: Learn Python: Inheritance and Polymorphism/Exercise 01: Inheritance.py
@@ -0,0 +1,10 @@
+# Think of inheritance as a remix — it sounds a lot like the original, but there’s something… different about it.
+# A base class is called a parent class.
+# In these terms, the class inheriting from it, the subclass, is also referred to as a child class.
+
+class Bin:
+  pass
+
+# Create subclass of Bin
+class RecyclingBin(Bin):
+  pass
