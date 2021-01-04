67be3bed51a0afaeb69c96c43116d52fb53e8c10
josejpalacios <jose.palacios6119@gmail.com>
2021-01-03 21:10:45 -0600

    Update Exercise 08: Constructors.py

--- Lesson 10: Classes/Lesson 01: Learn Python: Classes/Exercise 08: Constructors.py
+++ Lesson 10: Classes/Lesson 01: Learn Python: Classes/Exercise 08: Constructors.py
@@ -11,3 +11,15 @@
 # Methods that are used to prepare an object being instantiated are called constructors.
 # The word “constructor” is used to describe similar features in other object-oriented programming languages
 # but programmers who refer to a constructor in Python are usually talking about the __init__() method.
+
+# Parameters can be received by the __init__() method.
+class Circle:
+  pi = 3.14
+  
+  # Add constructor here:
+  def __init__(self, diameter):
+    # Print message
+    print('New circle with diameter: {diameter}'.format(diameter=diameter))
+
+# Create Circle instance
+teaching_table = Circle(36)
