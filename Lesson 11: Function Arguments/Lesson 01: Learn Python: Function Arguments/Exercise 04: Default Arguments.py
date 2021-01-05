86751d7141038c5d976e189df4b65ec9e06dd02e
josejpalacios <jose.palacios6119@gmail.com>
2021-01-04 19:03:10 -0600

    Create Exercise 04: Default Arguments.py

---
+++ Lesson 11: Function Arguments/Lesson 01: Learn Python: Function Arguments/Exercise 04: Default Arguments.py
@@ -0,0 +1,37 @@
+# Function arguments are required in Python.
+# So a standard function definition that defines two parameters
+# requires two arguments passed into the function.
+
+# Not all function arguments in Python need to be required.
+# If we give a default argument to a Python function that argument won’t be required when we call the function.
+
+import os
+
+# Add default value to nest
+def make_folders(folders_list, nest=False):
+  if nest:
+    """
+    Nest all the folders, like
+    ./Music/fun/parliament
+    """
+    path_to_new_folder = "."
+    for folder in folders_list:
+      path_to_new_folder += "/{}".format(folder)
+      try:
+        print(path_to_new_folder)
+        os.makedirs("./" + path_to_new_folder)
+      except FileExistsError:
+        continue
+  else:
+    """
+    Makes all different folders, like
+    ./Music/ ./fun/ and ./parliament/
+    """
+    for folder in folders_list:
+      try:
+        os.makedirs(folder)
+
+      except FileExistsError:
+        continue
+
+make_folders(['Music', 'fun', 'parliament'])
