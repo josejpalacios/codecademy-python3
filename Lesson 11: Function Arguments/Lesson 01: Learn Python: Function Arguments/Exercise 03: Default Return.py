e0abbb046d50e9e9e57a14a9ed4255889d181a53
josejpalacios <jose.palacios6119@gmail.com>
2021-01-04 19:00:48 -0600

    Update Exercise 03: Default Return.py

--- Lesson 11: Function Arguments/Lesson 01: Learn Python: Function Arguments/Exercise 03: Default Return.py
+++ Lesson 11: Function Arguments/Lesson 01: Learn Python: Function Arguments/Exercise 03: Default Return.py
@@ -11,3 +11,6 @@ print(prints_return)
 # call sort_this_list.sort() and save that in list_sort_return
 sort_this_list = [14, 631, 4, 51358, 50000000]
 list_sort_return = sort_this_list.sort()
+
+# print out the value of list_sort_return
+print(list_sort_return)
