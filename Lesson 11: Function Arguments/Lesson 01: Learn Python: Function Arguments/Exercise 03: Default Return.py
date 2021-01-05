33c9836c6a4e152bfc5cc13f9096882afa029ebd
josejpalacios <jose.palacios6119@gmail.com>
2021-01-04 18:59:56 -0600

    Create Exercise 03: Default Return.py

---
+++ Lesson 11: Function Arguments/Lesson 01: Learn Python: Function Arguments/Exercise 03: Default Return.py
@@ -0,0 +1,13 @@
+# A Python function that does not have any explicit return statement will return None after completing.
+
+# A return statement without any value returns None also.
+
+# store the result of this print() statement in the variable prints_return
+prints_return = print("What does this print function return?")
+
+# print out the value of prints_return
+print(prints_return)
+
+# call sort_this_list.sort() and save that in list_sort_return
+sort_this_list = [14, 631, 4, 51358, 50000000]
+list_sort_return = sort_this_list.sort()
